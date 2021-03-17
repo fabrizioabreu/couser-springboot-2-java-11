164a2b79ba40b25d88755cf56a30c834123e6340
Fabrizio Abreu <fabrizioabreu@live.com>
2021-03-17 16:15:43 -0300

    Exception handling - delete

--- src/main/java/com/nomedaempresa/course/services/UserService.java
+++ src/main/java/com/nomedaempresa/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.nomedaempresa.course.entities.User;
 import com.nomedaempresa.course.repositories.UserRepository;
+import com.nomedaempresa.course.services.exceptions.DatabaseException;
 import com.nomedaempresa.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,7 +35,14 @@ public class UserService {
 	
 	// DELETANDO usuario
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	// ATUALIZANDO usuario
